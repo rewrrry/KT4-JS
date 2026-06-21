533db46b007a269b571f0d27bfb79bcbdcba0f24
mawwka <166003344+rewrrry@users.noreply.github.com>
2026-06-22 08:19:09 +1000

    Create products.js

---
+++ products.js
@@ -0,0 +1,9 @@
+let products = [
+    {id: 1, title: 'велосипед', price: 45000, count: 3, marks_total: 15},
+    {id: 2, title: 'самокат', price: 15000, count: 5, marks_total: 20},
+    {id: 3, title: 'ролики', price: 8500, count: 12, marks_total: 36},
+    {id: 4, title: 'коньки', price: 12000, count: 8, marks_total: 16},
+    {id: 5, title: 'лыжи', price: 25000, count: 2, marks_total: 10}
+];
+
+module.exports = products;
